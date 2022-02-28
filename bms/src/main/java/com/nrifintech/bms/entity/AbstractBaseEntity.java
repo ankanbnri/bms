dc1dbf7f5c186212eaa43f66886f2b5d8390b8ce
AkashdeepNRIFT <100458212+AkashdeepNRIFT@users.noreply.github.com>
2022-02-28 12:28:02 +0530

    Create AbstractBaseEntity.java

---
+++ bms/src/main/java/com/nrifintech/bms/entity/AbstractBaseEntity.java
@@ -0,0 +1,39 @@
+package com.nrifintech.bms.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Version;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+
+@MappedSuperclass
+public class AbstractBaseEntity implements Serializable{
+
+
+	private static final long serialVersionUID = 1L;
+
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+  
+    @CreationTimestamp
+    @Column(updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt;
+    
+    @UpdateTimestamp
+    @Column(updatable = true)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updatedAt;
+
+    
+}
