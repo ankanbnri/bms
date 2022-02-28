9d7da28811ca7d4b9bfaa9f945ca52dcb0664e66
AkashdeepNRIFT <100458212+AkashdeepNRIFT@users.noreply.github.com>
2022-02-28 12:41:51 +0530

    Added User Repository

--- bms/src/main/java/com/nrifintech/bms/entity/AbstractBaseEntity.java
+++ bms/src/main/java/com/nrifintech/bms/entity/AbstractBaseEntity.java
@@ -4,13 +4,9 @@ import java.io.Serializable;
 import java.util.Date;
 
 import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
-import javax.persistence.Version;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
--- bms/src/main/java/com/nrifintech/bms/entity/User.java
+++ bms/src/main/java/com/nrifintech/bms/entity/User.java
@@ -2,30 +2,49 @@ package com.nrifintech.bms.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
-/**
- * @author	Debopam
- * @email	debpal07@gmail.com
- * @created	Nov 9, 2021
- */
+
 @Entity
-@Table(name="USER")
+@Table(name="user")
 public class User extends AbstractBaseEntity {
 	private static final long serialVersionUID = 6715094082833854125L;
 
-	@Column(name="USERNAME")
-	private String username;
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY) @Column(name="userid")
+    private Long userid;
 	
-	@Column(name="PASSWORD")
+	@Column(name="name")
+	private String name;
+	
+	@Column(name="password")
 	private String password;
+	
+	@Column(name="email")
+	private String email;
+	
+	@Column(name="phone_no")
+	private String mobileNo;
 
-	public String getUsername() {
-		return username;
+	@Column(name="isadmin")
+	private boolean adminPrivileges;
+
+	public Long getUserid() {
+		return userid;
 	}
 
-	public void setUsername(String username) {
-		this.username = username;
+	public void setUserid(Long userid) {
+		this.userid = userid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 
 	public String getPassword() {
@@ -35,4 +54,30 @@ public class User extends AbstractBaseEntity {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getMobileNo() {
+		return mobileNo;
+	}
+
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+
+	public boolean isAdminPrivileges() {
+		return adminPrivileges;
+	}
+
+	public void setAdminPrivileges(boolean adminPrivileges) {
+		this.adminPrivileges = adminPrivileges;
+	}
+	
+	
 }
