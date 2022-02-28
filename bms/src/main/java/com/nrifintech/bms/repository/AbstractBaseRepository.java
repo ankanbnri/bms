f075711392500ef98c7f3c38ef5a8dfd524e054c
AkashdeepNRIFT <100458212+AkashdeepNRIFT@users.noreply.github.com>
2022-02-28 12:38:51 +0530

    Added Abstract Base Repository

---
+++ bms/src/main/java/com/nrifintech/bms/repository/AbstractBaseRepository.java
@@ -0,0 +1,15 @@
+package com.nrifintech.bms.repository;
+
+import java.io.Serializable;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
+
+import com.nrifintech.bms.entity.AbstractBaseEntity;
+
+
+@NoRepositoryBean
+public interface AbstractBaseRepository<T extends AbstractBaseEntity, ID extends Serializable>
+extends JpaRepository<T, ID>{
+    
+}
