33819169aec97aa7a42942402004865e58d5c46e
AkashdeepNRIFT <100458212+AkashdeepNRIFT@users.noreply.github.com>
2022-03-02 15:20:38 +0530

    Added Admin Controller

---
+++ bms/src/main/java/com/nrifintech/bms/controller/AdminController.java
@@ -0,0 +1,18 @@
+package com.nrifintech.bms.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@RequestMapping("/admin")
+@Controller
+public class AdminController {
+
+	@GetMapping("/dashboard")
+	public ModelAndView getDashboard()
+	{
+		ModelAndView mv = new ModelAndView("AdminDashboard");
+		return mv;
+	}
+}
